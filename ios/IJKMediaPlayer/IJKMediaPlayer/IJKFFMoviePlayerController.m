cf16294ebf32aa89855b869dd57b3f0521d40a7d
Marty Tsai <marty.tsai@ulictek.com>
2020-10-30 13:56:46 +0800

    Change naming conflict

--- ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/IJKFFMoviePlayerController.m
@@ -42,7 +42,7 @@
 #import "ijkplayer/ijkavformat/ijkioapplication.h"
 #include "string.h"
 
-static const char *kIJKFFRequiredFFmpegVersion = "ffmpeg-020";
+static const char *kIJKFFRequiredFFmpegVersion = "ffmpeg-021";
 
 // It means you didn't call shutdown if you found this object leaked.
 @interface IJKWeakHolder : NSObject
